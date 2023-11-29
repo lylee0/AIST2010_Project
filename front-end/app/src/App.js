b8dec43ee27b3a2c41b6aa946a03c7962dba8aaf
Mitul Mohammad Abdullah Al Mukit <1155165211@link.cuhk.edu.hk>
2023-11-29 17:49:40 +0800

    Revert "11/25/2023 10:00pm"
    This reverts commit 7e59510591d67c68596e73578a56240a14b1d28f.

--- front-end/app/src/App.js
+++ front-end/app/src/App.js
@@ -1,7 +1,4 @@
-
-import React, { useState, useRef, useEffect } from 'react';
-import { CircleLoader } from 'react-spinners';
-//import WaveSurfer from 'wavesurfer.js';
+import React, { useState, useRef } from 'react';
 
 function App() {
   const [recording, setRecording] = useState(false);
@@ -9,8 +6,6 @@ function App() {
   const audioRef = useRef(null);
   const [audioChunks, setAudioChunks] = useState([]); 
   const chunks = [];
-
-  const [loading, setLoading] = useState(false);
 
   const startRecording = () => {
     navigator.mediaDevices.getUserMedia({ audio: true })
@@ -28,28 +23,34 @@ function App() {
           setAudioChunks(audioChunks);
           setRecording(false);
           sendAudioData(chunks);
-        });
+         });
+
+
+        setTimeout(() => {
+          mediaRecorder.stop();
+        }, 5000);
+
 
         setRecording(true);
-      })
-      .catch(error => {
-        console.error('Error accessing microphone:', error);
-        //eventually change error message to something in-page
-        alert('Error accessing microphone');
-      })
-    ;
+      });
   };
 
   const stopRecording = () => {
     if (mediaRecorder && mediaRecorder.state !== "inactive") {
-
       mediaRecorder.stop();
       
     }
   };
 
+  //Test purposes only
+  const playRecording = () => {
+    const audioBlob = new Blob(audioChunks, { type: 'audio/wav' });
+    const audioUrl = URL.createObjectURL(audioBlob);
+    audioRef.current.src = audioUrl; 
+    audioRef.current.play();
+  }
+
   const sendAudioData = (chunks) => {
-    setLoading(true);
     const audioBlob = new Blob(chunks, { type: 'audio/wav' });
 
     const formData = new FormData();
@@ -60,18 +61,10 @@ function App() {
       body: formData
       })
       .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        setLoading(false);
         // Handle response from backend if needed
         console.log('Audio data sent successfully');
       })
-      .then(data => {
-
-      })
       .catch(error => {
-        setLoading(false);
         // Handle error if the request fails
         console.error('Error sending audio data:', error);
       });
@@ -83,9 +76,10 @@ function App() {
         <h1>Singer Recognition</h1>
         {!recording && <button onClick={startRecording}>Start Recording</button>}
         {recording && <button onClick={stopRecording}>Stop Recording</button>}
-        
-        {loading && <CircleLoader />}
-        
+        <>
+          <button onClick={playRecording}>Play Recording</button>
+          <audio ref={audioRef} controls />
+        </>
       </header>
     </div>
   );
